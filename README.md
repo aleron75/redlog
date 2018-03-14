d614146de3d42e2045bae0907d4b4a0a5c539d7a
Alessandro Ronchi <aleron75@gmail.com>
2018-03-14 18:20:36 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# RedLog
+RedLod allows you to track time on Redmine via command line.
+
+## Installation
+The following instructions assume you use a \*nix terminal.
+
+Download the package or clone it, then run 
+
+    composer install
+
+After composer has finished downloading all the dependencies, give execution permissions to `redlog` file in root directory:
+
+    chmod +x redlog
+
+If you prefer, create a symlink to the `redlog` script in a shared path, for example
+
+    sudo ln -s /path/to/redlog_project/redlog /usr/bin/redlog    
+
+Copy the `.env.example` file into `.env` and customize it according to your Redmine settings.
+
+**Note:** customize letters of activities according to your preferences and bind them to your Reddmine's ids.
+
+## Usage
+From root directory, run the following command to track `2.5` hours for a development activity (d) on task `1234` done on 2018-03-14
+
+    redlog log 2018-03-14 2.5 1234 d "Fixed nasty bug"
+
+## Contributing
+Contributions are very welcome; refer to the [CONTRIBUTING.md](CONTRIBUTING.md) file for further details.
+
+## Licence
+This softwre is licensed under the **Open Software License version 3.0**.
+Refer to [LICENSE.txt](LICENSE.txt) file for further details.
---
+++ src/LogCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Aleron75\Redlog\Console;
+
+use Dotenv\Dotenv;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class LogCommand extends Command
+{
+    protected $activities = [];
+
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+
+        $dotenv = new Dotenv(__DIR__ . DIRECTORY_SEPARATOR . '..');
+        $dotenv->load();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('log')
+            ->setDescription('Log entries')
+            ->addArgument('date', InputArgument::REQUIRED)
+            ->addArgument('hours', InputArgument::REQUIRED)
+            ->addArgument('issue', InputArgument::REQUIRED)
+            ->addArgument('activity', InputArgument::REQUIRED)
+            ->addArgument('comment', InputArgument::OPTIONAL)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $date = $input->getArgument('date');
+        $hours = $input->getArgument('hours');
+        $issue = $input->getArgument('issue');
+        $activity = $input->getArgument('activity');
+        $comment = $input->getArgument('comment') ?: '';
+        $data = [
+            'issue_id' => $issue,
+            'spent_on' => $date,
+            'hours' => $hours,
+            'activity_id' => $this->getActivity($activity),
+            'comments' =>  htmlspecialchars($comment),
+        ];
+
+        $client = new \Redmine\Client(getenv('REDMINE_API_ENDPOINT'), getenv('REDMINE_API_TOKEN'));
+
+        // impersonate user
+        $client->setImpersonateUser(getenv('REDMINE_USER'));
+
+        // create a time entry for jsmith
+        $client->time_entry->create($data);
+
+        // remove impersonation for further calls
+        $client->setImpersonateUser(null);
+
+    }
+
+    private function getActivity($activity)
+    {
+        if (empty($this->activities)) {
+            $this->initActivities();
+        }
+        if (!isset($this->activities[$activity])) {
+            throw new \Exception('Activity ID for ' . $activity . ' not found!');
+            
+        }
+        return $this->activities[$activity];
+    }
+
+    private function initActivities()
+    {
+        $activities = getenv('REDMINE_ACTIVITIES');
+        if (empty($activities)) {
+            throw new \Exception('The REDMINE_ACTIVITIES in .env file is empty');
+        }
+
+        $activities = explode(',', $activities);
+        foreach ($activities as $kv) {
+            list($k, $v) = explode(':', $kv);
+            $this->activities[$k] = $v;
+        }
+    }
+}
